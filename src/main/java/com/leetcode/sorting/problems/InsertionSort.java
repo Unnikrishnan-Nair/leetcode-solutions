1acff2393a2c5a8b2a9ffe2e522dc03841c36da2
Unnikrishnan-nair <ukpnair@gmail.com>
2020-07-02 18:16:15 -0700

    Adding problems

--- src/main/java/com/leetcode/sorting/problems/InsertionSort.java
+++ src/main/java/com/leetcode/sorting/problems/InsertionSort.java
@@ -1,17 +1,22 @@
 package com.leetcode.sorting.problems;
 
 public class InsertionSort {
-	
-	public void performSort(int[] array) {
-		for(int i=1; i<array.length; i++) {
-			int key = array[i];
-			int j=i-1;
-			while(j >= 0 && array[j] > key) {
-				array[j+1] = array[j];
-				j=j-1;
-			}
-			array[j + 1] = key; 
-		}
-	}
+
+    /**
+     * 
+     * @param array
+     */
+    public void performSort(int[] array) {
+        for (int i = 1; i < array.length; i++) {
+            int key = array[i];
+            int j = i - 1;
+            while (j >= 0 && array[j] > key) {
+                int temp = array[j + 1];
+                array[j + 1] = array[j];
+                array[j] = temp;
+                j--;
+            }
+        }
+    }
 
 }
--- src/main/java/com/leetcode/sorting/problems/MergeSort.java
+++ src/main/java/com/leetcode/sorting/problems/MergeSort.java
@@ -1,64 +1,64 @@
 package com.leetcode.sorting.problems;
 
 public class MergeSort {
-	
-	public int[] mergeSort(int[] array) {
-		
-		if(array.length < 2) {
-			return array;
-		}
-		
-		int middle = array.length/2;
-		
-		int[] leftArray = new int[middle];
-		int[] rightArray = null;
-		
-		if(array.length % 2 == 0) {
-			rightArray = new int[middle];
-		}else {
-			rightArray = new int[middle+1];
-		}
-		
-		for(int i=0; i< leftArray.length; i++) {
-			leftArray[i]= array[i];
-		}
-		
-		for(int i=0; i< rightArray.length; i++) {
-			rightArray[i] = array[middle + i];
-		}
-		
-		leftArray = mergeSort(leftArray);
-		rightArray = mergeSort(rightArray);
-		
-		int[] result = mergeHalves(leftArray,rightArray);
-		
-		return result;
-		
-	}
 
-	private int[] mergeHalves(int[] leftArray, int[] rightArray) {
-		
-		int leftPtr = 0;
-		int rightPtr = 0;
-		int resultPtr = 0;
-		int[] resultArray = new int[leftArray.length+rightArray.length];
-		
-		while(leftPtr < leftArray.length || rightPtr < rightArray.length) {
-			
-			if(leftPtr < leftArray.length && rightPtr < rightArray.length) {
-				if(leftArray[leftPtr] < rightArray[rightPtr]) {
-					resultArray[resultPtr++] = leftArray[leftPtr++];
-					
-				}else {
-					resultArray[resultPtr++] = rightArray[rightPtr++];
-				}
-			}else if(leftPtr < leftArray.length) {
-				resultArray[resultPtr++] = leftArray[leftPtr++];
-			}else {
-				resultArray[resultPtr++] = rightArray[rightPtr++];
-			}
-		}
-		return resultArray;
-	}
+    public int[] mergeSort(int[] array) {
+
+        if (array.length < 2) {
+            return array;
+        }
+
+        int middle = array.length / 2;
+
+        int[] leftArray = new int[middle];
+        int[] rightArray = null;
+
+        if (array.length % 2 == 0) {
+            rightArray = new int[middle];
+        } else {
+            rightArray = new int[middle + 1];
+        }
+
+        for (int i = 0; i < leftArray.length; i++) {
+            leftArray[i] = array[i];
+        }
+
+        for (int i = 0; i < rightArray.length; i++) {
+            rightArray[i] = array[middle + i];
+        }
+
+        leftArray = mergeSort(leftArray);
+        rightArray = mergeSort(rightArray);
+
+        int[] result = mergeHalves(leftArray, rightArray);
+
+        return result;
+
+    }
+
+    private int[] mergeHalves(int[] leftArray, int[] rightArray) {
+
+        int leftPtr = 0;
+        int rightPtr = 0;
+        int resultPtr = 0;
+        int[] resultArray = new int[leftArray.length + rightArray.length];
+
+        while (leftPtr < leftArray.length || rightPtr < rightArray.length) {
+
+            if (leftPtr < leftArray.length && rightPtr < rightArray.length) {
+                if (leftArray[leftPtr] < rightArray[rightPtr]) {
+                    resultArray[resultPtr++] = leftArray[leftPtr++];
+
+                } else {
+                    resultArray[resultPtr++] = rightArray[rightPtr++];
+                }
+            } else if (leftPtr < leftArray.length) {
+                resultArray[resultPtr++] = leftArray[leftPtr++];
+            } else {
+                resultArray[resultPtr++] = rightArray[rightPtr++];
+            }
+        }
+        return resultArray;
+    }
 
 }
